83d18b5b04896d2d8284615c79dab35b58ac5428
samanthadiaz6460-del <samantha.diaz6460@alumnos.udg.mx>
2026-05-12 18:25:04 -0600

    baje el peso de varias cosas
     para poder hacer build

---
+++ Assets/MINIJUEGOS/Scripts/MEMORY/Editor/CardManagerEditor.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CardManager))]
+public class CardManagerEditor : Editor
+{
+    SerializedObject manager;
+    SerializedProperty _pairAmount;
+    SerializedProperty _width;
+    SerializedProperty _height;
+    SerializedProperty _spriteList;
+
+    int spriteAmount;
+
+    void OnEnable()
+    {
+        manager = new SerializedObject(target);
+        _pairAmount = manager.FindProperty("pairAmount");
+        _width = manager.FindProperty("width");
+        _height = manager.FindProperty("height");
+        _spriteList = manager.FindProperty("spriteList");
+
+        spriteAmount = _spriteList.arraySize;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        manager.Update();
+
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+
+        GUI.enabled = false;
+        EditorGUILayout.PropertyField(_pairAmount);
+        GUI.enabled = true;
+
+        EditorGUILayout.PropertyField(_width);
+        EditorGUILayout.PropertyField(_height);
+
+        float tmp = _width.intValue * (float)_height.intValue / 2;
+        _pairAmount.intValue = Mathf.CeilToInt(tmp);
+
+        if (_pairAmount.intValue > spriteAmount)
+        {
+            EditorGUILayout.HelpBox("Demasiados pares para la cantidad de sprites", MessageType.Error);
+        }
+
+        if (_width.intValue < 0) _width.intValue = 0;
+        if (_height.intValue < 0) _height.intValue = 0;
+
+        EditorGUILayout.EndVertical();
+
+        manager.ApplyModifiedProperties();
+        DrawDefaultInspector();
+    }
+}
